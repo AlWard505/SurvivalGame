58ebb91cdd1393b23695dc9f4512b187446d1372
epicpigslav688 <60785111+epicpigslav688@users.noreply.github.com>
2021-03-21 23:06:29 +0000

    Add files via upload

--- savesystem.py
+++ savesystem.py
@@ -2,6 +2,7 @@
 from worldgeneration import generation
 from playerdata import data
 import os
+from json import *
 from tkinter import *
 def New(filename):
     check = 0
@@ -32,40 +33,9 @@ def New(filename):
     data(filename)
     return filename
 #continue
-def Continue(filename):
-    check = 0
-    x = 0
-    badthings = '*."/\\[];:|,'
-    namehelp = ""
-    linecount = 1
-    save = open("saves/savenames.txt","a")
-    save.close
-    print("these are your current saves")
-    save = open("saves/savenames.txt","r")
-    for line in save:
-        print(str(line).translate({ord("\n"): None}))
-        if line != "\n":
-            linecount += 1
-    linecount+=-1
-    file = input("please enter which file you wish to open: ").translate({ord(" "): None})
-    linecount+=1
-    while file != int:
-        try:
-            file = int(file)
-            while file >= linecount or file <= 0:
-                file = input("please enter one of the presented numbers: ").translate({ord(" "): None})
-                file = int(file)
-            break
-        except:
-            file = input("please enter one of the presented numbers: ").translate({ord(" "): None})
-        execute
-    save.close
-    save = open("saves/savenames.txt","r")
-    while x != file:
-        line = save.readline()
-        x+=1
-    while line[0]!=")":
-        line= line.translate({ord(line[0]): None})
-    filename = line.translate({ord(line[0]): None})
-    filename = filename.translate({ord("\n"): None})
-    return filename
+def Save(filename,stuff,world):
+    with open("saves/"+filename+"/"+filename+"data.json", 'w') as outfile:
+        dump(stuff, outfile)
+    with open("saves/"+filename+"/"+filename+"worlddata.json", 'w') as outfile:
+        dump(world, outfile)
+    
